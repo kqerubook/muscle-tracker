782ac475e8b29312430445d1dcc063c4462638fe
fujino kakeru <fujinokakeru@fujinonoMacBook-Air.local>
2026-05-11 00:37:21 +0900

    Initial commit: muscle tracker PWA

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'muscle-v1';
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.add('./'); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) { return r || fetch(e.request); })
+  );
+});
